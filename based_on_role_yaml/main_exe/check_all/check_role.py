c70870c0aeba20aff7d5cea5a767bee2f580fbcb
gjimmyz <85646830@qq.com>
2023-09-01 10:45:19 +0800

    202309011045

--- based_on_role_yaml/main_exe/check_all/check_role.py
+++ based_on_role_yaml/main_exe/check_all/check_role.py
@@ -15,9 +15,13 @@ import hashlib
 import argparse
 
 os.environ['PATH'] = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
 scripts_path = "/root/scripts/"
-roles_file_path = scripts_path + "AutoDeploy360/based_on_role_yaml/roles_to_include.yaml"
-playbook_path = scripts_path + "AutoDeploy360/based_on_role_yaml/playbook_name.yaml"
+roles_file_path = os.path.join(scripts_path, "AutoDeploy360/based_on_role_yaml/roles_to_include.yaml")
+playbook_path = os.path.join(scripts_path, "AutoDeploy360/based_on_role_yaml/playbook_name.yaml")
+custom_hosts_default_filename = "check_customize_hosts.yaml"
+custom_hosts_default_path = os.path.join(scripts_path, "AutoDeploy360/based_on_role_yaml/main_exe/check_all/", custom_hosts_default_filename)
+
 now = datetime.now()
 year = now.strftime("%Y")
 month = now.strftime("%m")
@@ -48,6 +52,11 @@ def get_facts(custom_hosts=None):
     inventory_file = 'get_real_can_loginhost_for_create_hosts_yaml/files/hosts.yaml' if not custom_hosts else custom_hosts
     subprocess.run(['ansible-playbook', '-i', inventory_file, '/root/scripts/AutoDeploy360/based_on_role_yaml/get_facts.yaml'], check=True)
 
+def get_custom_hosts_from_input():
+    print(f"Suggested filename: {custom_hosts_default_filename}")
+    custom_hosts_input = input("Enter the custom host filename (or press Enter to use the default): ")
+    return os.path.join(scripts_path, "AutoDeploy360/based_on_role_yaml/main_exe/check_all/", custom_hosts_input) if custom_hosts_input else None
+
 def send_mail():
     file_paths = []
     for root, dirs, files in os.walk(output_dir):
@@ -71,7 +80,11 @@ def send_mail():
 
 if __name__ == "__main__":
     args = parse_args()
-    generate_hosts(custom_hosts=args.custom_hosts)
-    get_facts(custom_hosts=args.custom_hosts)
+    use_custom = input("Would you like to use a custom configuration? (yes/no): ")
+    if use_custom.lower() == 'yes':
+        custom_hosts = get_custom_hosts_from_input()
+    else:
+        custom_hosts = None
+    generate_hosts(custom_hosts=custom_hosts)
+    get_facts(custom_hosts=custom_hosts)
     send_mail()
-
